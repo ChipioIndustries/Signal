818f381433189fdebc724d27bd36b6d3924aa63b
Chipio Industries <53354220+ChipioIndustries@users.noreply.github.com>
2022-02-04 09:41:54 -0800

    Unit test signal.lua

--- src/init.spec.lua
+++ src/init.spec.lua
@@ -1,7 +1,31 @@
 return function()
 	describe("Signal.lua", function()
-		it("empty", function()
-		
+		local Signal = require(script.Parent)
+
+		it("Should create and fire a signal", function()
+			local result = 0
+
+			local function onSignal(value)
+				task.wait(0.2)
+				result += value
+			end
+
+			local signal = Signal.new()
+
+			signal:connect(onSignal)
+			signal:connect(onSignal)
+
+			expect(function()
+				signal:connect()
+			end).to.throw()
+
+			signal:fire(3)
+
+			expect(result).to.equal(0)
+
+			task.wait(0.3)
+
+			expect(result).to.equal(6)
 		end)
 	end)
 end
